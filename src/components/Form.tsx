98921a82039028dd813bfafed2255302a7217a11
Munna Kumar <mkcodes.in@gmail.com>
2025-11-14 12:17:26 +0530

    starting form page

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
 export default function Form() {
   return (
-    <div>
-      
+    <div className="p-4 bg-gray-700">
+      <form>
+        <div>
+          <label htmlFor="description">Description</label>
+          <input id="description" type="text" />
+        </div>
+        <div>
+          <label htmlFor="amount">Amount</label>
+          <input type="text" name="amount" id="amount" />
+        </div>
+        <div>
+          <label htmlFor="category">Categery</label>
+          <input type="text" name="category" id="category" />
+        </div>
+        <div>
+          <label htmlFor="date">Date</label>
+          <input type="date" id="date" />
+        </div>
+        <button type="submit">Add Expense</button>
+      </form>
     </div>
   )
 }
