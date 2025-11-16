e5584041c383827777b59d46a43b7a389890ef2f
Munna Kumar <mkcodes.in@gmail.com>
2025-11-16 11:46:35 +0530

    History section

---
+++ src/components/DashboardHistory.tsx
@@ -0,0 +1,7 @@
+export default function DashboardHistory() {
+  return (
+    <div>
+      History
+    </div>
+  )
+}
--- src/components/DashboardMain.tsx
+++ src/components/DashboardMain.tsx
@@ -1,14 +1,18 @@
 import Chart from './Chart'
 import DashboardHead from './DashboardHead'
+import DashboardHistory from './DashboardHistory'
 import Form from './Form'
 
 export default function DashboardMain() {
   return (
-    <div className='max-w-7xl mx-auto px-4 py-4'>
+    <div className='flex flex-col gap-8 max-w-7xl mx-auto px-4 py-4'>
       <DashboardHead />
-      <div className='mt-4 grid grid-cols-[1fr_auto] gap-3'>
+      <div className='grid grid-cols-[1fr_auto] gap-3'>
         <div><Chart /></div>
         <div><Form /></div>
+      </div>
+      <div>
+        <DashboardHistory />
       </div>
     </div>
   )
