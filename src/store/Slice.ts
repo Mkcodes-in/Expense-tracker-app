810d9e886b0758d4012054a925181b7d77778716
Munna Kumar <mkcodes.in@gmail.com>
2025-11-23 09:56:18 +0530

    localstorage implement

--- src/store/Slice.ts
+++ src/store/Slice.ts
@@ -1,20 +1,29 @@
 import type { Expense } from "@/types/Expense";
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
-const initialState: Expense[] = [];
+const storedExpenses =
+  typeof window !== "undefined"
+    ? JSON.parse(localStorage.getItem("expenses") || "[]")
+    : [];
+
+const initialState: Expense[] = storedExpenses;
 
 const ExpenseTrack = createSlice({
-    name: 'Add_Expense',
-    initialState,
-    reducers: {
-        add_expense: (state, action: PayloadAction<Expense>) => {
-            state.push(action.payload)
-        }, 
-        remove_expense: (state, action) => {
-            return state.filter(itm => itm.id !== action.payload);
-        }
-    }
+  name: "Add_Expense",
+  initialState,
+  reducers: {
+    add_expense: (state, action: PayloadAction<Expense>) => {
+      state.push(action.payload);
+      localStorage.setItem("expenses", JSON.stringify(state));
+    },
+
+    remove_expense: (state, action) => {
+      const newState = state.filter((itm) => itm.id !== action.payload);
+      localStorage.setItem("expenses", JSON.stringify(newState));
+      return newState;
+    },
+  },
 });
 
-export const {add_expense, remove_expense} = ExpenseTrack.actions;
+export const { add_expense, remove_expense } = ExpenseTrack.actions;
 export default ExpenseTrack.reducer;
